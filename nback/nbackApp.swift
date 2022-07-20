25e3cad5d9e93229f88a4cb5cdf7b3bb9c2f37c8
DendyGrobovshik <idenis.gradoboev@yandex.com>
2022-07-20 19:23:57 +0500

    Initial Commit

---
+++ nback/nbackApp.swift
@@ -0,0 +1,17 @@
+//
+//  nbackApp.swift
+//  nback
+//
+//  Created by Denis Gradoboev on 20.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct nbackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
